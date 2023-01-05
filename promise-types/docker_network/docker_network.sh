0b4afb01e319dbff1fa79e670f1ece38141ca822
Dennis Stam <dennis.stam@surf.nl>
2023-01-05 15:09:36 +0100

    Docker network Added a simple promise type to create/remove docker networks

---
+++ promise-types/docker_network/docker_network.sh
@@ -0,0 +1,45 @@
+required_attributes="state"
+optional_attributes=""
+all_attributes_are_valid="no"
+
+
+LOG_PREFIX="${0##*/}"
+
+do_validate() {
+    response_result="valid"
+}
+
+do_evaluate() {
+
+    local result
+
+    # 0 does not exist, if >0 it exists
+    result=$(docker network inspect ${request_promiser} 2>/dev/null | jq '. | length')
+
+    # Default the promise is alwasys 'kept'
+    response_result="kept"
+
+
+    log debug "${LOG_PREFIX}:${request_promiser} result: ${result}"
+
+    case "${request_attribute_state}" in
+        absent)
+            if [[ ${result} -ne 0 ]]
+            then
+                result=$(docker network rm ${request_promiser})
+                response_result="repaired"
+            fi
+        ;;
+        present)
+            if [[ ${result} -eq 0 ]]
+            then
+                result=$(docker network create ${request_promiser})
+                response_result="repaired"
+            fi
+        ;;
+    esac
+
+}
+
+. "$(dirname "$0")/cfengine.sh"
+module_main "docker_network" "1.0"
